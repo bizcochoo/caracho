4b928da9c821e08acc5d54d4b4fd5d115ae5a412
bizcochoo <109878481+bizcochoo@users.noreply.github.com>
2024-09-06 09:10:25 -0400

    goto, prompt para calidad de video
    syntax

--- bin/spotdl.bat
+++ bin/spotdl.bat
@@ -1,6 +1,14 @@
 @echo off
+:a
 echo.
-echo Ingresa URL de Spotify:
+echo Ingresa URL de Spotify [Pista/Album/Playlist]:
 set /p "input= "
 .\spotdl.exe --output "./../output" %input%
+
+echo Desea descargar mas musica? [y/n]:
+set /p "bool= "
+if "%bool%" == "y" (goto :a) else (goto :b)
+
+:b
+echo.
 pause
--- bin/yt-dlp.bat
+++ bin/yt-dlp.bat
@@ -1,4 +1,5 @@
 @echo off
+:a
 echo.
 echo Ingresa URL de Youtube:
 set /p "input="
@@ -6,8 +7,20 @@ echo Seleccione formato:
 echo [VIDEO: .mp4 .webm] [AUDIO: .mp3 .wav .opus .m4a]
 set /p "tipo="
 
+set "cal=0"
+if "%tipo%" == ".mp4" set "cal=1"
+if "%tipo%" == ".webm" set "cal=1"
+if "%cal%"=="1" (
+    echo Seleccione calidad:
+    echo [CALIDAD: 1080 720 480 360 240 144]
+    set /p "calidad="
+)
+
+
 if "%tipo%"==".webm" (
-    .\yt-dlp.exe -P "./../output" "%input%"
+    .\yt-dlp -P "./../output" -S res:"%calidad%" "%input%"
+) else if "%tipo%"==".mp4" (
+    .\yt-dlp.exe -P "./../output" -S res:"%calidad%",ext:mp4:m4a --recode mp4 "%input%"
 ) else if "%tipo%"==".opus" (
     .\yt-dlp -P "./../output" -x "%input%"
 ) else if "%tipo%"==".mp3" (
@@ -16,8 +29,13 @@ if "%tipo%"==".webm" (
     .\yt-dlp -P "./../output" -x --audio-format wav --audio-quality 0 "%input%"
 ) else if "%tipo%"==".m4a" (
     .\yt-dlp -P "./../output" -x --audio-format m4a --audio-quality 0 "%input%"
-) else if "%tipo%"==".mp4" (
-    .\yt-dlp -P "./../output" -S res,ext:mp4:m4a --recode mp4 "%input%"
 )
 
+echo.
+echo Desea descargar otro archivo? [y/n]:
+set /p "bool= "
+if "%bool%" == "y" (goto :a) else (goto :b)
+
+:b
+echo.
 pause
